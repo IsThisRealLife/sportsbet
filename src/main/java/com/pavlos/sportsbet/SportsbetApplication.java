db477fbb084f8291b67cf0c1943acb5c9fd463b3
Pavlos Kapoutsis <pavloskapoutsis@gmail.com>
2021-09-17 12:55:43 +0300

    project init

---
+++ src/main/java/com/pavlos/sportsbet/SportsbetApplication.java
@@ -0,0 +1,13 @@
+package com.pavlos.sportsbet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsbetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsbetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pavlos/sportsbet/SportsbetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavlos.sportsbet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsbetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
